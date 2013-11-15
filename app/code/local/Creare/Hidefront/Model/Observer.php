2950cab616e8e71f4e69a611394f5f471f0bdd08
Adam Moss <adammoss7@gmail.com>
2013-11-15 10:20:37 +0000

    First commit - all files

---
+++ app/code/local/Creare/Hidefront/Model/Observer.php
@@ -0,0 +1,18 @@
+<?php
+
+class Creare_Hidefront_Model_Observer
+{
+    public function hideFront()
+    {
+        
+        if (Mage::getStoreConfig('dev/restrict/hidefrontend'))
+        {
+            $ips = explode(",", Mage::getStoreConfig('dev/restrict/allow_ips'));
+
+            if (!in_array($_SERVER['REMOTE_ADDR'], $ips))
+            {
+                die(Mage::getStoreConfig('dev/restrict/message'));
+            }
+        }
+    }
+}
